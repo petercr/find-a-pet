198d10274dba28b9e72b9e52a189067d7c6a6b24
Peter <petecruckshank@gmail.com>
2018-07-15 21:38:39 -0400

    wrote the main info to fetch API data and log it

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-alert("yo");
+// PetFinder API key = c5f90e0fc44747991721516ca355f18f
+
+import fetchJsonp from 'fetch-jsonp';
+
+const petForm = document.querySelector('#pet-form');
+
+petForm.addEventListener('submit', fetchAnimals);
+
+// Fetch animals from API
+function fetchAnimals(e) {
+    e.preventDefault();
+
+    const animal = document.querySelector('#animal').value;
+    const zip = document.querySelector('#zip').value;
+
+    // fetch pets
+    fetchJsonp(`http://api.petfinder.com/pet.find?format=json&key=c5f90e0fc44747991721516ca355f18f&animal=${animal}&location=${zip}&callback=callback`, {
+        jsonpCallbackFunction: 'callback'
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+}
+
+// jsonp Callback
+function callback(data) {
+    console.log(data);
+}
