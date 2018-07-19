7cf0b7696dfad9363e6a99be8cfc6fd241f024cd
Peter <petecruckshank@gmail.com>
2018-07-18 21:47:23 -0400

    parcel.js run && build files

--- dist/main.c3688d98.js
+++ dist/main.c3688d98.js
@@ -352,7 +352,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '44909' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '42811' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
