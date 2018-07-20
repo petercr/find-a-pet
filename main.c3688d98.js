ca109b6b7d6a58c382c131047cbef53856d1c7cd
Peter <petecruckshank@gmail.com>
2018-07-19 21:19:18 -0400

    Updates

--- main.c3688d98.js
+++ main.c3688d98.js
@@ -291,7 +291,7 @@ function fetchAnimals(e) {
 
   // fetch pets
   // fetch(`http://api.petfinder.com/pet.find?format=json&key=c5f90e0fc44747991721516ca355f18f&animal=${animal}&location=${zip}`)
-  (0, _fetchJsonp2.default)("http://api.petfinder.com/pet.find?format=json&key=c5f90e0fc44747991721516ca355f18f&animal=" + animal + "&location=" + zip + "&callback=callback", {
+  (0, _fetchJsonp2.default)("https://api.petfinder.com/pet.find?format=json&key=c5f90e0fc44747991721516ca355f18f&animal=" + animal + "&location=" + zip + "&callback=callback", {
     jsonpCallbackFunction: "callback"
   }).then(function (res) {
     return res.json();
@@ -319,7 +319,7 @@ function showAnimals(pets) {
     console.dir(pet);
     var div = document.createElement("div");
     div.classList.add("card", "card-body", "mb-3");
-    div.innerHTML = "\n            <div class=\"row\">\n                <div class=\"col-sm-6\">\n                    <h4>" + pet.name.$t + " (" + pet.age.$t + ")</h4>\n                    <p class=\"text-secondary\">" + (pet.breeds.breed.$t ? pet.breeds.breed.$t : pet.breeds.breed["0"].$t) + "</p>\n                    <p>" + pet.contact.address1.$t + " " + pet.contact.city.$t + " " + pet.contact.state.$t + " " + pet.contact.zip.$t + "</p>\n                    <ul class=\"list-group ml-3\">\n                        <li class=\"list-group-item\">Phone: " + pet.contact.phone.$t + "</li>\n                        " + (pet.contact.email.$t ? "<li class=\"list-group-item\">Email: " + pet.contact.email.$t + "</li>" : "") + "\n                        <li class=\"list-group-item\">Shelter ID: " + pet.shelterId.$t + "</li>\n                    </ul>\n                </div>\n                <div class=\"col-sm-6 text-center\">\n                    <img class=\"img-fluid rounded-circle mt-2\" src=\"" + pet.media.photos.photo[2].$t + "\">\n                </div>\n                <div class=\"col-sm-12 mt-2\">\n                  <p><b>About:</b> " + pet.description.$t + "</p>\n                </div>\n            </div>\n        ";
+    div.innerHTML = "\n            <div class=\"row\">\n                <div class=\"col-sm-6\">\n                    <h4>" + pet.name.$t + " (" + pet.age.$t + ")</h4>\n                    <p class=\"text-secondary\">" + (pet.breeds.breed.$t ? pet.breeds.breed.$t : pet.breeds.breed["0"].$t) + "</p>\n                    <p>" + pet.contact.address1.$t + " " + pet.contact.city.$t + " " + pet.contact.state.$t + " " + pet.contact.zip.$t + "</p>\n                    <ul class=\"list-group ml-3\">\n                        <li class=\"list-group-item\">Phone: " + pet.contact.phone.$t + "</li>\n                        " + (pet.contact.email.$t ? "<li class=\"list-group-item\">Email: " + pet.contact.email.$t + "</li>" : "") + "\n                        <li class=\"list-group-item\">Shelter ID: " + pet.shelterId.$t + "</li>\n                    </ul>\n                </div>\n                <div class=\"col-sm-6 text-center\">\n                    <img class=\"img-fluid rounded mt-2\" src=\"" + pet.media.photos.photo[2].$t + "\">\n                </div>\n                <div class=\"col-sm-12 mt-4\">\n                  <p style=\"font-family: 'Merienda', cursive;\"><b>About:</b> " + pet.description.$t + "</p>\n                </div>\n            </div>\n        ";
     results.appendChild(div);
   });
 }
@@ -352,7 +352,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '42811' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '34059' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
